d71cec2ef2a648afa93904ab3f231741d4e874a5
Jillian Shao <jillian.j.shao@gmail.com>
2026-02-25 13:14:36 -0500

    Add example usage of ... in biomass_index

--- R/biomass.R
+++ R/biomass.R
@@ -4,6 +4,9 @@
 #'
 #' @param cpue Numeric vector of CPUE values
 #' @param area_swept Numeric vector of area swept (e.g., km²)
+#' @param catch Numeric value of catch
+#' @param effort Numeric value of effort
+#' @param ... Arguments passed to cpue()
 #'
 #' @return A numeric vector of biomass index values
 #' @export
@@ -12,6 +15,15 @@
 #' salmon_cpue <- cpue(catch = 2, effort = 2)
 #' biomass_index(cpue = salmon_cpue, area_swept = 5)
 
-biomass_index <- function(cpue, area_swept) {
+biomass_index <- function(
+  cpue = NULL,
+  area_swept,
+  catch = NULL,
+  effort = NULL,
+  ...
+) {
+  if (is.null(cpue) && (!is.null(catch) && !is.null(effort))) {
+    cpue <- cpue(catch, effort, ...)
+  }
   cpue * area_swept
 }
