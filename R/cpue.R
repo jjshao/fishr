b1ec0ba19c3cb0a357991b180c369c882468a3bf
Jillian Shao <jillian.j.shao@gmail.com>
2026-02-25 12:49:32 -0500

    Add option for verbosity

--- R/cpue.R
+++ R/cpue.R
@@ -5,7 +5,7 @@
 #' @param catch Numeric vector of catch (e.g., kg)
 #' @param effort Numeric vector of effort (e.g., hours)
 #' @param gear_factor Numeric adjustment for gear standardization (default is 1)
-#' @param verbose Logical indicating whether to print processing messages (default is FALSE)
+#' @param verbose Logical indicating whether to print processing messages (default is FALSE, also accept the value of 'fishr.verbose')
 #'
 #' @returns A numeric vector of CPUE values
 #' @export
@@ -14,7 +14,12 @@
 #' cpue(100, 10)
 #' cpue(100, 10, gear_factor = 0.5)
 
-cpue <- function(catch, effort, gear_factor = 1, verbose = FALSE) {
+cpue <- function(
+  catch,
+  effort,
+  gear_factor = 1,
+  verbose = getOption("fishr.verbose", default = FALSE)
+) {
   if (verbose) {
     message("Processing ", length(catch), " records")
   }
